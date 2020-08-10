a394259a2fbf96578c01b807f40171d78e679e2d
Santhosh <santhoshjee0@gmail.com>
2020-08-10 13:00:57 +0530

    Removed code from onInstalled

--- background.js
+++ background.js
@@ -10,50 +10,50 @@ async function postData(url = "", data = {}) {
   return response.json();
 }
 
+chrome.webNavigation.onDOMContentLoaded.addListener((details) => {
+  if(details.frameId==0){
+    let urlData = {
+      fields: {
+        url: {
+          stringValue: details.url,
+        },
+        timestamp: {
+          timestampValue: new Date(details.timeStamp),
+        },
+      },
+    };
+    chrome.storage.sync.get(["monitoring_urls"], function (data) {
+
+      if (
+        data &&
+        data["monitoring_urls"] &&
+        data["monitoring_urls"].length &&
+        data["monitoring_urls"].indexOf(new URL(details.url).origin) !== -1
+      ) {
+         chrome.storage.sync.get(['firebaseUrl'],(data)=>{
+          let web_events_url = data?.firebaseUrl
+           if(!web_events_url){
+             alert("Please configure firebase URL in plugin options to log data to firebase")
+           }else{
+              postData(web_events_url, urlData).then((res) =>
+                console.log("posted navigation and got response", res)
+              );
+           }
+         })
+      }
+    });
+  }
+});
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+  chrome.declarativeContent.onPageChanged.addRules([
+    {
+      conditions: [new chrome.declarativeContent.PageStateMatcher({})],
+      actions: [new chrome.declarativeContent.ShowPageAction()],
+    },
+  ]);
+});
+
 chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set({ monitoring_urls: [] }, function () {});
-
-  chrome.webNavigation.onDOMContentLoaded.addListener((details) => {
-    if(details.frameId==0){
-      let urlData = {
-        fields: {
-          url: {
-            stringValue: details.url,
-          },
-          timestamp: {
-            timestampValue: new Date(details.timeStamp),
-          },
-        },
-      };
-      chrome.storage.sync.get(["monitoring_urls"], function (data) {
-
-        if (
-          data &&
-          data["monitoring_urls"] &&
-          data["monitoring_urls"].length &&
-          data["monitoring_urls"].indexOf(new URL(details.url).origin) !== -1
-        ) {
-           chrome.storage.sync.get(['firebaseUrl'],(data)=>{
-            let web_events_url = data?.firebaseUrl
-             if(!web_events_url){
-               alert("Please configure firebase URL in plugin options to log data to firebase")
-             }else{
-                postData(web_events_url, urlData).then((res) =>
-                  console.log("posted navigation and got response", res)
-                );
-             }
-           })
-        }
-      });
-    }
-  });
-
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [new chrome.declarativeContent.PageStateMatcher({})],
-        actions: [new chrome.declarativeContent.ShowPageAction()],
-      },
-    ]);
-  });
 });
